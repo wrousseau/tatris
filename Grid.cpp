06a157304faae323f5fd36d143f664395263fda6
Manchel <simon_manchel@hotmail.fr>
2013-01-29 11:32:13 +0100

    Veritable ajout des .cpp :)

---
+++ Grid.cpp
@@ -0,0 +1,43 @@
+#include "Grid.h"
+
+
+bool Grid::isInBounds(int par1, int par2) {
+    if (par1 <= GRID_WIDTH*25 && par1 >= 0 && par2 <= GRID_HEIGHT*25 && par2 >= 0) {
+
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+bool Grid::isFree(point par1)
+{
+    if (isInBounds(par1.x1,par1.x2) && isNotOccupied(par1)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+
+bool Grid::isNotOccupied(point par1) {
+    if(area[par1.x1][par1.x2] == EMPTY)
+        return true;
+    else
+        return false;
+}
+
+bool Grid::isLineFull(int line){
+    bool test = true;
+    int i=0;
+
+    while(test)
+    {
+        if(area[i][line] == EMPTY)
+            test = false;
+        i++;
+    }
+    return test;
+}
